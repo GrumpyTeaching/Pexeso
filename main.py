37775462b159edd584a1ff067f0a8efd76f9a6b2
GrumpyTeaching <17488583-GrumpyTeaching@users.noreply.replit.com>
2023-04-17 14:10:01 +0000

    mouse clicks

--- main.py
+++ main.py
@@ -19,12 +19,28 @@ def create_cards():
 
 cards = create_cards()
 random.shuffle(cards)
+reversed = []
 
-def draw_card(i):
-    row = i // COLUMNS
-    column = i % COLUMNS
+# 0 .. 5
+
+def position_to_index(pos):
+    (x, y) = pos
+    row = y // (SIZE+5) 
+    column = x // (SIZE+5)
+    idx = row * COLUMNS + column
+    print(f"{x}, {y} -> {idx}")
+    return idx
+
+def draw_card(idx):
+    row = idx // COLUMNS
+    column = idx % COLUMNS
     pos = (column * (SIZE+5), row * (SIZE+5))
-    screen.blit(cards[i], pos)    
+    screen.blit(cards[idx], pos)
+    # cover
+    rect = (pos, (SIZE, SIZE))
+    color = (40, 40, 40)
+    if idx not in reversed:
+        pygame.draw.rect(screen, color, rect)
 
 def draw_game():
     screen.fill(background)
@@ -34,10 +50,21 @@ def draw_game():
 
 def wait_event():
     global running
+    global reversed
     event = pygame.event.wait()
     if event.type == pygame.QUIT:
         running = False
-
+    elif event.type == pygame.MOUSEBUTTONDOWN:
+        pos = pygame.mouse.get_pos()
+        idx = position_to_index(pos)
+        if idx < 12:
+            if len(reversed) == 0:
+                reversed.append(idx)
+            elif len(reversed) == 1:
+                reversed.append(idx)
+            else:
+                reversed = []
+    
 def run():
     pygame.display.set_caption('Hello World!')
     while running:
